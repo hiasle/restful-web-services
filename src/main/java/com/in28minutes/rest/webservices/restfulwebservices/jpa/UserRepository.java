a4d71527d90d91bb6e2d90d3c86e4828da712321
Matthias Huber <matthias.hube@gmail.com>
2022-09-26 23:14:36 +0200

    user repo custom query

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jpa/UserRepository.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jpa/UserRepository.java
@@ -3,5 +3,10 @@ package com.in28minutes.rest.webservices.restfulwebservices.jpa;
 import com.in28minutes.rest.webservices.restfulwebservices.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    List<User> findUserByName(String name);
+
 }
